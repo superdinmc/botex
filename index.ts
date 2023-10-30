e333e4f5a7f5cba5852300e34e0b955657b5196f
5UP3R_D1N <superdinmc@gmail.com>
2023-10-30 09:47:21 +0000

    fixes

--- index.ts
+++ index.ts
@@ -1,20 +1,29 @@
 import {parse} from 'yaml'
 import * as fs from 'fs';
-import {Client} from 'discord.js';
+import {Client,Partials,GatewayIntentBits} from 'discord.js';
 if(!fs.existsSync('config.yaml')) {
     console.error('Error: config.yaml not found');
     process.exit(1);
 }
 const config = parse(fs.readFileSync('config.yaml').toString());
 const bot = new Client({
-    intents: 'GuildMessages',
-    shards: 'auto'
+    intents: [
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent
+    ],
+    shards: 'auto',
+    partials: [
+        Partials.Message
+    ]
 })
 bot.on('ready',()=>{
     console.log('Logged in as '+bot.user.username)
 });
 bot.on('messageCreate',message=>{
     try {
+        if(message.author.bot) return;
         const cont = message.content;
         const fn = config.responses.find((e: {regex: string,flag?: string})=>(new RegExp(e.regex,e.flag||'mi')).test(cont));
         if(!fn) return;
@@ -23,7 +32,7 @@ bot.on('messageCreate',message=>{
         if(fn.action) eval(fn.action);
         if(fn.response) {
             const result = eval('`'+fn.response+'`'); //for ${} supports, although this is exploitable, but these codes are only accessible by the bot owner anyways.
-            message.reply(result);
+            message.reply(result).catch(console.error);
         }
     } catch(e) {console.error(e);}
 })
