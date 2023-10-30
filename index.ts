038851959ee05ce7e3a5b385abcb13525cdbfd6c
superdinmc <superdinmc@gmail.com>
2023-10-30 15:09:31 +0700

    error catches

--- index.ts
+++ index.ts
@@ -14,12 +14,19 @@ bot.on('ready',()=>{
     console.log('Logged in as '+bot.user.username)
 });
 bot.on('messageCreate',message=>{
-    const cont = message.content;
-    const fn = config.responses.find((e: {regex: string,flag?: string})=>(new RegExp(e.regex,e.flag||'mi')).test(cont));
-    if(!fn) return;
-    const match = cont.match(new RegExp(fn.regex));
-    const _: object = {...message,match};
-    const result = eval('`'+fn.response+'`'); //for ${} supports, although this is exploitable, but these codes are only accessible by the bot owner anyways.
-    message.reply(result);
+    try {
+        const cont = message.content;
+        const fn = config.responses.find((e: {regex: string,flag?: string})=>(new RegExp(e.regex,e.flag||'mi')).test(cont));
+        if(!fn) return;
+        const match = cont.match(new RegExp(fn.regex));
+        const _: object = {...message,match};
+        if(fn.action) eval(fn.action);
+        if(fn.response) {
+            const result = eval('`'+fn.response+'`'); //for ${} supports, although this is exploitable, but these codes are only accessible by the bot owner anyways.
+            message.reply(result);
+        }
+    } catch(e) {console.error(e);}
 })
+bot.on('error',console.error);
+process.on('uncaughtException',console.error);
 bot.login(config.token);
